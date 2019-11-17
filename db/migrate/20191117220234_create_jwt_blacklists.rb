153c29ea1c7dbbccece5c63f63eb8e8f601351c2
quesurifn <kyle.c.r.fahey@gmail.com>
2019-11-17 18:42:34 -0500

    Fix migrations

--- db/migrate/20191117220234_create_jwt_blacklists.rb
+++ db/migrate/20191117220234_create_jwt_blacklists.rb
@@ -1,7 +1,7 @@
 class CreateJwtBlacklists < ActiveRecord::Migration[6.0]
   def change
     create_table :jwt_blacklists do |t|
-      t.token :string
+      t.string :token
       t.integer :expiration
       t.timestamps
     end
