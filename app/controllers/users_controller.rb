5db66b91c577ccff6dcd356288b6de2db7f0508e
quesurifn <kyle.c.r.fahey@gmail.com>
2019-11-17 18:46:03 -0500

    Add missing colon in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     def update
         unless @user.update(user_params)
-            render json: {errors @user.errors.fill_messages}, status: :unprocessable_entity
+            render json: {errors: @user.errors.fill_messages}, status: :unprocessable_entity
         end
     end
 
